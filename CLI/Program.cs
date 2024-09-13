88afa1864e40edd7bb1335c35e61f143dbc36f1d
NikkyAS <nikolai64000@gmail.com>
2024-09-13 10:44:31 +0200

    More changes to part 2

---
+++ CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using CLI.UI;
+using InMemoryRepositories;
+using RepositoryContracts;
+
+Console.WriteLine("Starting CLI app..");
+IUserRepository userRepository = new UserInMemoryRepository();
+ICommentRepository commentRepository = new CommentInMemoryRepository();
+IPostRepository postRepository = new PostInMemoryRepository();
+
+CliApp cliApp = new CliApp();
+await cliApp.StartAsync();
