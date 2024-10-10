fcf439ed79f31e27a820422ced05ae54f7e7b7d8
FREDERIK DIAMOND <hello@frederikhandberg.com>
2024-10-10 12:41:44 +0200

    CommentFileRepository implemented

--- FileRepositories/CommentFileRepository.cs
+++ FileRepositories/CommentFileRepository.cs
@@ -18,41 +18,70 @@ public class CommentFileRepository : ICommentRepository
     
     public async Task<Comment> AddAsync(Comment comment)
     {
-        string commentsAsJson = await File.ReadAllTextAsync(filePath);
-        List<Comment> comments = JsonSerializer.Deserialize<List<Comment>>(commentsAsJson)!;
-        int maxId = comments.Count > 0 ? comments.Max(c => c.Id) : 1;
+        List<Comment> comments = await LoadCommentsAsync();
+        int maxId = comments.Count > 0 ? comments.Max(c => c.Id) : 0;
         comment.Id = maxId + 1;
         comments.Add(comment);
-        commentsAsJson = JsonSerializer.Serialize(comments);
-        await File.WriteAllTextAsync(filePath, commentsAsJson);
+        await SaveCommentsAsync(comments);
         return comment;
     }
-    
-    // Below methods need to be implemented:
 
-    public Task UpdateAsync(Comment comment)
+    public async Task UpdateAsync(Comment comment)
     {
-        throw new NotImplementedException();
+        List<Comment> comments = await LoadCommentsAsync();
+        int index = comments.FindIndex(c => c.Id == comment.Id);
+        if (index != -1)
+        {
+            comments[index] = comment;
+            await SaveCommentsAsync(comments);
+        }
+        else
+        {
+            throw new KeyNotFoundException($"Comment with ID {comment.Id} not found.");
+        }
     }
 
-    public Task DeleteAsync(int id)
+    public async Task DeleteAsync(int id)
     {
-        throw new NotImplementedException();
+        List<Comment> comments = await LoadCommentsAsync();
+        Comment commentToRemove = comments.FirstOrDefault(c => c.Id == id);
+        if (commentToRemove != null)
+        {
+            comments.Remove(commentToRemove);
+            await SaveCommentsAsync(comments);
+        }
+        else
+        {
+            throw new KeyNotFoundException($"Comment with ID {id} not found.");
+        }
     }
 
-    public Task<Comment> GetSingleAsync(int id)
+    public async Task<Comment> GetSingleAsync(int id)
     {
-        throw new NotImplementedException();
+        List<Comment> comments = await LoadCommentsAsync();
+        Comment comment = comments.FirstOrDefault(c => c.Id == id);
+        if (comment == null)
+        {
+            throw new KeyNotFoundException($"Comment with ID {id} not found.");
+        }
+        return comment;
     }
-
-    /*public IQueryable<Comment> GetMany()
-    {
-        throw new NotImplementedException();
-    }*/
     
     public IQueryable<Comment> GetMany() {
         string commentsAsJson = File.ReadAllTextAsync(filePath).Result;
         List<Comment> comments = JsonSerializer.Deserialize<List<Comment>>(commentsAsJson)!;
         return comments.AsQueryable();
+    }
+    
+    private async Task<List<Comment>> LoadCommentsAsync()
+    {
+        string commentsAsJson = await File.ReadAllTextAsync(filePath);
+        return JsonSerializer.Deserialize<List<Comment>>(commentsAsJson) ?? new List<Comment>();
+    }
+
+    private async Task SaveCommentsAsync(List<Comment> comments)
+    {
+        string commentsAsJson = JsonSerializer.Serialize(comments);
+        await File.WriteAllTextAsync(filePath, commentsAsJson);
     }
 }
