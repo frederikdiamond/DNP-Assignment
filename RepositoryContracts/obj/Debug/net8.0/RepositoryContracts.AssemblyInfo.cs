ee0f98e29e0896c90386cf77287b0a7f0e2e07e3
NikkyAS <nikolai64000@gmail.com>
2024-10-10 13:52:30 +0200

    Still some issues with the "view post" as it doesn't display all details

--- RepositoryContracts/obj/Debug/net8.0/RepositoryContracts.AssemblyInfo.cs
+++ RepositoryContracts/obj/Debug/net8.0/RepositoryContracts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RepositoryContracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+590b56db07f0332ca1903e160841c587a5a05b4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97c40a9e729f75bc3e97fff5da2727b9e383e55c")]
 [assembly: System.Reflection.AssemblyProductAttribute("RepositoryContracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RepositoryContracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
